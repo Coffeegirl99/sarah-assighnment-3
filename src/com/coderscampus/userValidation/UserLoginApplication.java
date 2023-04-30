eec888a99cea380f5430f415b5952adfce7927ba
Sarah Lagares <smlagares99@gmail.com>
2023-04-30 09:21:07 -0400

    V2

--- src/com/coderscampus/userValidation/UserLoginApplication.java
+++ src/com/coderscampus/userValidation/UserLoginApplication.java
@@ -1,10 +1,13 @@
 package com.coderscampus.userValidation;
 
+import java.io.IOException;
+
 public class UserLoginApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		
+		UserService loginApplication = new UserService();
+		 loginApplication.validateUser("data.txt");
 	}
 
 }
--- src/com/coderscampus/userValidation/UserService.java
+++ src/com/coderscampus/userValidation/UserService.java
@@ -1,6 +1,51 @@
 package com.coderscampus.userValidation;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class UserService {
 
-	
+	public void validateUser(String fileName) throws IOException {
+
+		BufferedReader fileReader = null;
+
+		try {
+			fileReader = new BufferedReader(new FileReader(fileName));
+			String data;
+			
+			System.out.println("Enter your email:");
+			
+			Scanner scanner = new Scanner(System.in);
+			String userInput = scanner.nextLine();
+			
+			System.out.println("Enter your password:");
+			userInput = scanner.nextLine();
+			
+			while ((data = fileReader.readLine()) != null) {
+				String[] userArray = data.split(",");
+				String username = userArray[0];
+				String password = userArray[1];
+				String name = userArray[2];
+			}
+
+		} finally {
+			if (fileReader != null) {
+				try {
+					fileReader.close();
+				} catch (IOException e) {
+
+				}
+			}
+		}
+	}
+
+	private User createJobs(String username, String password, String name) {
+		User users = new User();
+		users.setUsername(username);
+		users.setPassword(password);
+		users.setName(name);
+		return users;
+	}
 }
