04de45789cc44ab6e8c9930d44d88f5d7244073c
Sarah Lagares <smlagares99@gmail.com>
2023-08-07 18:49:36 -0400

    V6

--- src/com/coderscampus/application/UserLoginApplication.java
+++ src/com/coderscampus/application/UserLoginApplication.java
@@ -9,9 +9,9 @@ import com.coderscampus.service.UserService;
 public class UserLoginApplication {
 
 	public static void main(String[] args) throws IOException {
- 
+
 		UserService userService = new UserService();
-		
+
 		try {
 			List<User> users = userService.loadUsersFromFile("data.txt");
 			login(users);
@@ -21,11 +21,11 @@ public class UserLoginApplication {
 	}
 
 	public static void login(List<User> users) {
-		
+
 		try (Scanner scanner = new Scanner(System.in)) {
 			int userAttempts = 0;
 
-			while (userAttempts < 5) {
+			while (userAttempts <= 5) {
 				System.out.println("Enter your email:");
 				String userInputEmail = scanner.nextLine();
 				System.out.println("Enter your password:");
@@ -35,15 +35,14 @@ public class UserLoginApplication {
 
 				if (matchedUser != null) {
 					System.out.println("Welcome: " + matchedUser.getName());
-					break; // Terminate the application
+					return; // Terminate the application
 				} else {// if it does not matched
 					System.out.println("Invalid login, please try again");
 					userAttempts++;
 				}
 			}
+			// User used all attempts, so they get locked out
+			System.out.println("Too many failed login attempts, you are now locked out.");
 		}
-		// User used all attempts, so they get locked out
-		System.out.println("Too many failed login attempts, you are now locked out.");
 	}
-
 }
